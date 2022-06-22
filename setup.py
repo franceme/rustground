a8b61816f03c83a392417c4162ecee678a3fca19
franceme <frantzme@vt.edu>
2022-06-22 02:49:58 +0000

    Initial Commit

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # region Imports
-import pathlib, zipfile
+import pathlib,
 from fileinput import FileInput as finput
 import os
 import sys
@@ -8,24 +8,11 @@ from setuptools import find_packages, setup
 from pathlib import Path
 import glob
 from glob import glob as re
-try:
-	from pylint.reporters.json_reporter import JSONReporter
-except:
-	pass
-try:
-	from src.information import VERSION, REQ
-except:
-	pass
-try:
-	from information import VERSION, REQ
-except:
-	pass
-
 # endregion
 # region Basic Information
 here = os.path.abspath(os.path.dirname(__file__))
 py_version = sys.version_info[:2]
-NAME = ""
+NAME = "rustground"
 AUTHOR = 'Miles Frantz'
 EMAIL = 'frantzme@vt.edu'
 DESCRIPTION = 'My short description for my project.'
@@ -34,33 +21,7 @@ URL = f"https://github.com/{GH_NAME}/{NAME}"
 long_description = pathlib.Path(f"{here}/README.md").read_text(encoding='utf-8')
 REQUIRES_PYTHON = '>=3.8.0'
 RELEASE = "?"
-entry_point = f"src.{NAME}"
-VERSION = "0.0.0"
-
-def zip_program(outputName:str = f"{NAME}.zip"):
-	#http://blog.ablepear.com/2012/10/bundling-python-files-into-stand-alone.html
-	if os.path.exists(outputName):
-		os.system(f"rm {outputName}")
-
-	zipf = zipfile.ZipFile(outputName, 'w', zipfile.ZIP_DEFLATED)
-	success = 0
-	try:
-		zipf.write("setup.py")
-		zipf.write("README.md")
-		zipf.write("__main__.py")
-		for root, dirs, files in os.walk('src/'):
-			for file in [x for x in files if not x.endswith('.pyc')]:
-				ending_path = os.path.relpath(os.path.join(root, file), os.path.join('src/', '..'))
-				zipf.write(
-					os.path.join(root, file),
-					ending_path
-				)
-		print(f"Successful: {outputName}")
-	except Exception as e:
-		print(f"Failing the exception check: {e}")
-		success = 1
-	zipf.close()
-	return(success)
+VERSION = "0.0.1"
 
 def grab_version(update_patch:bool=False,update_minor:bool=False,update_major:bool=False):
 	update = any([update_patch,update_minor,update_major])
@@ -98,8 +59,6 @@ if selfArg('install'):
 elif selfArg('upload'):
 	grab_version(True)
 	sys.exit(os.system(f"{sys.executable} setup.py sdist && {sys.executable} -m twine upload --skip-existing dist/*"))
-elif selfArg('zip'):
-	sys.exit(zip_program())
 # endregion
 # region Setup
 
@@ -118,15 +77,15 @@ setup(
 	packages=find_packages(
 		exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
 	entry_points={
+		'console_scripts': ['mycli=rustground.__main__:main'],
 	},
 	install_requires=[
-		REQ
+		"requests"
 	],
 	include_package_data=True,
 	classifiers=[
 		'Programming Language :: Python',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.8',
+		'Programming Language :: Python :: 3'
 	],
 )
 # endregion
